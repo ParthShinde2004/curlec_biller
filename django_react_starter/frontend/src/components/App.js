1dcbc9e9bc1e4039e2ba29d752a0d8102e4cbab6
ParthShinde2004 <204789@gardenschool.edu.my>
2021-06-25 14:45:01 +0800

    added buttons

--- django_react_starter/frontend/src/components/App.js
+++ django_react_starter/frontend/src/components/App.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import Header from "./Header";
 import LeftForm from "./LeftForm";
 import RightForm from "./RightForm";
+import Buttons from "./Buttons";
 
 const App = () => {
   return (
@@ -10,9 +11,15 @@ const App = () => {
       <Header />
       <LeftForm />
       <RightForm />
+      <Buttons />
     </div>
   );
 };
 
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
+
+/*
+to do:
+1. when you open console, settings button change
+*/
---
+++ django_react_starter/frontend/src/components/Buttons.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export class Buttons extends Component {
+  render() {
+    return (
+      <div className="d-grid gap-2 col-6 mx-auto">
+        <button className="btn btn-primary" type="button">
+          Export to Zip
+        </button>
+        <button className="btn btn-primary" type="button">
+          Export to Excel
+        </button>
+        <button className="btn btn-primary" type="button">
+          List of Live Merchants
+        </button>
+        <button className="btn btn-primary" type="button">
+          List of Non-Live Merchants
+        </button>
+      </div>
+    );
+  }
+}
+export default Buttons;
+
+/*
+to do:
+*/
