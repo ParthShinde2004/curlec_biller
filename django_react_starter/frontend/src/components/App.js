b73265f381c73665f5c27d0d08a4dce774ad209b
ParthShinde2004 <204789@gardenschool.edu.my>
2021-06-28 16:15:50 +0800

    deleted unused files (buttons.js)

--- django_react_starter/frontend/src/components/App.js
+++ django_react_starter/frontend/src/components/App.js
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import Header from "./Header";
 import LeftForm from "./LeftForm";
 import RightForm from "./RightForm";
-import Buttons from "./Buttons";
 
 const App = () => {
   return (
@@ -12,9 +11,6 @@ const App = () => {
       <div style={{ float: "left", width: 500, padding: 40 }}>
         <LeftForm />
       </div>
-      {/* <div style={{ float: "left", width: 500 }}>
-        <Buttons />
-      </div> */}
       <div style={{ float: "middle", width: 500, padding: 40 }}>
         <RightForm />
       </div>
